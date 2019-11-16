7487c1e0c67d73f23e16809aa33cd17fc6e51e12
vimaljyothi-it19 <54206485+vimaljyothi-it19@users.noreply.github.com>
2019-11-16 11:35:06 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,15 @@
+def speed(x):
+  if(x<=70):
+    c="ok"
+  else:
+    d=x-70
+    e=d//5
+    if(e<=12):
+      c="point : %d"%e
+    else:
+      c="your license suspeded"
+  return c;
+#example
+print(speed(80))
+print(speed(60))
+print(speed(135))
